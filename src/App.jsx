a8e1e21e5b749e9861c1d82f857eefde795fa0c1
Chirag Kashyap <chirag@Mac.lan>
2025-10-29 22:45:07 +0530

    made container vertically centered for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,14 @@ import './App.css'
 import { v4 as uuidv4 } from 'uuid';
 
   function App() {
+
   const [todo, setTodo] = useState("")
   const [todos, setTodos] = useState([])
   const [checked, setchecked] = useState(true)
 
-
   const handleChange = (e) => {
     setTodo(e.target.value)
-  
+
   }
 
   const handleAdd = () => {
@@ -33,7 +33,6 @@ import { v4 as uuidv4 } from 'uuid';
     }
   }
   
-
   const handleEdit = (e) => {
     let id = e.target.name
     let newtodos = todos.map(item=>{
@@ -45,6 +44,7 @@ import { v4 as uuidv4 } from 'uuid';
     })
     setTodos(newtodos)
   }
+
   const handleDelete = (e) => {
     
     if (confirm("do you wanna delete this task")){
@@ -78,69 +78,73 @@ import { v4 as uuidv4 } from 'uuid';
   return ( 
     <>
     <Navbar/>
-    <div className="container bg-purple-200 h-[80vh] m-auto mt-8 rounded-4xl px-10 py-5 box-border">
-      <div className='title text-[2.8rem] text-center pb-5 font-medium'>justDo - Manage your todos at one place</div>
 
-      <div className='addATodo '>
-        <div className='text-[2.5rem] pb-4 '>Add a Todo</div>
-        <div className='flex gap-4 '>
-          <input onChange={handleChange} onKeyDown={handleEnter} value={todo} className='border text-[1.8rem] px-5 border-pink-300 bg-pink-200 rounded-4xl w-[90%] h-18' type="text" />
-          <button onClick={handleAdd} className='text-[2.2rem] my-button px-5'>Save</button>
-        </div>
-      </div>
+    <div className="bigcontainer flex justify-center items-center h-[80vh] sm:h-[83vh]">
+      
+      <div className="Container w-[80vw] sm:w-[50vw]  bg-purple-200 min-h-[70vh] sm:min-h-[80vh]  mt-8 rounded-4xl px-10 py-5 box-border">
+        <div className='title text-[2.8rem] text-center pb-5 font-medium'>justDo - Manage your todos at one place</div>
 
-      {todos.length!=0 && <div className="showcomplete flex gap-3 items-center mt-8">
-        <input checked={checked} onChange={handleFinishTasks} type="checkbox" id='checkbox' />
-        <label htmlFor="checkbox" className='text-[1.8rem]'>Show finished Tasks</label>
-      </div>}
-
-      <div className="line w-[50vw] mt-9 border m-auto border-slate-400 rounded-4xl"></div>
-
-      {todos.length==0 && <div className='text-[2.5rem] text-center pt-4'>No todos to display</div>}
-
-      {todos.length!=0 && <div className="yourtodos flex flex-col gap-4">
-        <div className='text-[2.5rem] pb-4 pt-4 '>Your Todos</div>
-
-        {todos.map(item=>{
-          if(checked){
-
-          return(
-          
-          <div key={item.id} className="all flex justify-between">
-            <div className="inputlabel flex items-center gap-3 text-[1.5rem]">
-              <input checked={item.isCompleted}  onChange={handleComplete} type="checkbox" id={item.id} />
-              <label name={item.id} className={item.isCompleted? "line-through":""} htmlFor={item.id}>{item.todo}</label>
-            </div>
-        
-            <div className="buttons flex text-[1.5rem] gap-3">
-              <button onClick={handleEdit} name={item.id} className="edit my-button px-5">Edit</button>
-              <button onClick={handleDelete} name={item.id} className="delete my-button px-5">Delete</button>
-            </div>
-          </div>) 
-          }else if(item.isCompleted!==true){
-            
-            return(
-              
-              <div key={item.id} className="all flex justify-between">
-            <div className="inputlabel flex items-center gap-3 text-[1.5rem]">
-              <input checked={item.isCompleted}  onChange={handleComplete} type="checkbox" id={item.id} />
-              <label name={item.id} className={item.isCompleted? "line-through":""} htmlFor={item.id}>{item.todo}</label>
-            </div>
-        
-            <div className="buttons flex text-[1.5rem] gap-3">
-              <button onClick={handleEdit} name={item.id} className="edit my-button px-5">Edit</button>
-              <button onClick={handleDelete} name={item.id} className="delete my-button px-5">Delete</button>
-            </div>
+        <div className='addATodo '>
+          <div className='text-[2.5rem] pb-4 '>Add a Todo</div>
+          <div className='flex gap-4 '>
+            <input onChange={handleChange} onKeyDown={handleEnter} value={todo} className='border text-[1.8rem] px-5 border-pink-300 bg-pink-200 rounded-4xl w-[90%] h-18' type="text" />
+            <button onClick={handleAdd} className='text-[2.2rem] my-button px-5'>Save</button>
           </div>
-            )
-          }
-          
-        })}
+        </div>
 
-      </div>}
+        {todos.length!=0 && <div className="showcomplete flex gap-3 items-center mt-8">
+          <input checked={checked} onChange={handleFinishTasks} type="checkbox" id='checkbox' />
+          <label htmlFor="checkbox" className='text-[1.8rem]'>Show finished Tasks</label>
+        </div>}
+
+        <div className="line w-[38vw] mt-9 border m-auto border-slate-400 rounded-4xl"></div>
+
+        {todos.length==0 && <div className='text-[2.5rem] text-center pt-4'>No todos to display</div>}
+
+        {todos.length!=0 && <div className="yourtodos flex flex-col gap-4">
+          <div className='text-[2.5rem] pb-4 pt-4 '>Your Todos</div>
+
+          {todos.map(item=>{
+            if(checked){
+
+            return(
+            
+            <div key={item.id} className="all flex justify-between">
+              <div className="inputlabel flex items-center gap-3 text-[1.5rem]">
+                <input checked={item.isCompleted}  onChange={handleComplete} type="checkbox" id={item.id} />
+                <label name={item.id} className={item.isCompleted? "line-through":""} htmlFor={item.id}>{item.todo}</label>
+              </div>
+          
+              <div className="buttons flex text-[1.5rem] gap-3">
+                <button onClick={handleEdit} name={item.id} className="edit my-button px-5">Edit</button>
+                <button onClick={handleDelete} name={item.id} className="delete my-button px-5">Delete</button>
+              </div>
+            </div>) 
+            }else if(item.isCompleted!==true){
+              
+              return(
+                
+                <div key={item.id} className="all flex justify-between">
+              <div className="inputlabel flex items-center gap-3 text-[1.5rem]">
+                <input checked={item.isCompleted}  onChange={handleComplete} type="checkbox" id={item.id} />
+                <label name={item.id} className={item.isCompleted? "line-through":""} htmlFor={item.id}>{item.todo}</label>
+              </div>
+          
+              <div className="buttons flex text-[1.5rem] gap-3">
+                <button onClick={handleEdit} name={item.id} className="edit my-button px-5">Edit</button>
+                <button onClick={handleDelete} name={item.id} className="delete my-button px-5">Delete</button>
+              </div>
+            </div>
+              )
+            }
+            
+          })}
+
+        </div>}
+      </div>
     </div>
+
     </>
   ) 
 } 
 export default App
-
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,13 +4,13 @@ import todoIcon from './todoicon.png';
 
 const Navbar = () => {
     return (
-        <nav className="bg-blue-200">
+        <nav className="bg-blue-200 w-[100vw]">
             <ul className="flex text-[2.4rem] justify-between items-center">
-                <li className="flex gap-4 px-30 items-center h-[5rem] hover:bg-blue-300 cursor-pointer hover:text-[2.7rem] transition-all">
+                <li className="flex gap-4 px-15 sm:px-30 items-center h-[5rem] hover:bg-blue-300 cursor-pointer hover:text-[2.7rem] transition-all">
                     <span>justDo</span>
                     <img className="w-[4rem]" src={todoIcon}/>
                 </li>
-                <div className="flex items-center mr-[3.75rem]">
+                <div className="hidden  sm:flex sm:items-center sm:mr-[3.75rem]">
                     <li className="h-[5rem] px-15 flex items-center hover:bg-blue-300 cursor-pointer hover:text-[2.7rem] transition-all">Home</li>
                     <li className="h-[5rem] px-15 flex items-center hover:bg-blue-300 cursor-pointer hover:text-[2.7rem] transition-all">Your Tasks</li>
                 </div>
